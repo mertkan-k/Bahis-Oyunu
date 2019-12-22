25916b08ec1f87986e0040a1855736e5873a16e8
Mertkan Kececi <47403024+mertkan-k@users.noreply.github.com>
2019-12-23 00:44:42 +0300

    initial

--- main.h
+++ main.h
@@ -22,7 +22,7 @@ int my_atoi(const char* snum);//https://stackoverflow.com/questions/7021725/how-
 const int startingMoney[2] = {400, 700};// min, max
 const int finishMoney = 25000;
 const int raceLenghts[2] = {45,85};// min, max
-const char carNames[__CAR_COUNT+1]="RMZKLMNABCD";
+const char carNames[__CAR_COUNT+1]="RM";
 
 int yatirilanPara = 0; int yatirilanAraba = 0;
 int randoms[__CAR_COUNT][10];
